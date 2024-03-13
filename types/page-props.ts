b95bcab86c8fa811acaad0d564968ff4d1dac63a
Marcus0086 <guptamarcus42@gmail.com>
2024-03-14 02:14:09 +0530

    Add isLive and accountType properties to PageProps

--- types/page-props.ts
+++ types/page-props.ts
@@ -1,15 +1,20 @@
 import { ExtendedRecordMap } from "notion-types";
+import { AccountType } from "@prisma/client";
+
 import { PageError } from "./page-error";
 import { Site } from "./site";
 import { SiteConfig } from "./site-config";
 import { ParsedUrlQuery } from "querystring";
 import { SiteMap } from "./site-map";
+
 export interface PageProps {
   site?: Site;
   recordMap?: ExtendedRecordMap;
   pageId?: string;
   error?: PageError;
   config?: SiteConfig;
+  isLive?: boolean;
+  accountType?: AccountType;
 }
 
 export interface ProviderPageProps extends PageProps {
