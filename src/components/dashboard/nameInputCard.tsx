d9c5039bc6cb462901993029a5fa7379a2d3340a
Marcus0086 <guptamarcus42@gmail.com>
2024-03-16 02:15:13 +0530

    Add tooltip functionality to NameInputCard component

--- src/components/dashboard/nameInputCard.tsx
+++ src/components/dashboard/nameInputCard.tsx
@@ -1,6 +1,13 @@
 import { VisibilityFilter } from "@prisma/client";
+import { MdInfoOutline } from "react-icons/md";
 
 import CardInput from "@/components/dashboard/cardInput";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 import { cn } from "@/lib/utils";
 
@@ -17,6 +24,7 @@ const NameInputCard = ({
   siteId,
   siteTitle,
   siteDescription,
+  toolTip,
 }: {
   title: string;
   description?: string;
@@ -29,15 +37,30 @@ const NameInputCard = ({
   siteId: string;
   siteTitle: string;
   siteDescription: string;
+  toolTip?: string;
 }>) => {
   return (
     <li
       className={cn(
         "bg-white dark:bg-navy-800 shadow w-full rounded-xl",
-        "p-4 flex flex-col items-start justify-between gap-y-3 font-normal"
+        "p-4 flex flex-col items-start justify-between gap-y-3 font-normal",
       )}
     >
-      <h3 className="text-cloudBurst dark:text-white text-base">{title}</h3>
+      <div className="flex items-center justify-center gap-x-2">
+        <h3 className="text-cloudBurst dark:text-white text-base">{title}</h3>
+        {toolTip ? (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <MdInfoOutline />
+              </TooltipTrigger>
+              <TooltipContent className="bg-white border border-fog drop-shadow-sm">
+                <p className="text-gray-800">{toolTip}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ) : null}
+      </div>
       <p className="text-gray-700 text-xs">{description}</p>
       <CardInput
         type={type || "text"}
