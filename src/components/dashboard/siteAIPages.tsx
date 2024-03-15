d20d3ece7def6fa2bf806b766946691c32eca06f
Marcus0086 <guptamarcus42@gmail.com>
2024-03-16 02:15:24 +0530

    Refactor siteAIPages component to use infinite query

--- src/components/dashboard/siteAIPages.tsx
+++ src/components/dashboard/siteAIPages.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Fragment, useMemo, useState } from "react";
 import { toast } from "react-toastify";
 
 import { DataTable } from "@/components/dashboard/dataTable";
@@ -19,39 +19,60 @@ import { Document } from "@/types/kb";
 const SiteAIPages = ({ siteId }: { siteId: string }) => {
   const [isTraining, setIsTraining] = useState(false);
   const toastOptions = useToast();
-  const { data: siteDocuments } = useQuery({
+  const {
+    data: infiniteSiteDocuments,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
     queryKey: ["ai", siteId],
-    queryFn: () => getSiteDocuments(siteId),
-    placeholderData: null,
+    queryFn: ({ pageParam }) => getSiteDocuments(siteId, pageParam, 4),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
     staleTime: Infinity,
   });
 
-  if (!siteDocuments) return null;
+  const canonicalPageMap = useMemo(() => {
+    return infiniteSiteDocuments?.pages.flatMap(
+      (page) => page.canonicalPageMap
+    );
+  }, [infiniteSiteDocuments?.pages]);
 
-  const { canonicalPageMap, documents } = siteDocuments;
+  const documents = useMemo(() => {
+    return infiniteSiteDocuments?.pages.flatMap((page) => page.documents);
+  }, [infiniteSiteDocuments?.pages]);
 
   const handleKbTraining = async () => {
     let kbDocuments: Document[] = [];
-    documents.forEach((document) => {
-      kbDocuments = kbDocuments.concat(document.subDocuments);
-    });
-    setIsTraining(true);
-    toast.info("Training KB...", toastOptions);
-    const response = await trainKnowledgeBases(kbDocuments, siteId);
-    if (response && response.success) {
-      toast.success(response.success, toastOptions);
-    } else {
-      toast.error(
-        "Something went wrong!" + " " + response?.error,
-        toastOptions
-      );
+    if (documents) {
+      documents.forEach((document) => {
+        kbDocuments = kbDocuments.concat(document.subDocuments);
+      });
+      setIsTraining(true);
+      toast.info("Training KB...", toastOptions);
+      const response = await trainKnowledgeBases(kbDocuments, siteId);
+      if (response && response.success) {
+        toast.success(response.success, toastOptions);
+      } else {
+        toast.error(
+          "Something went wrong!" + " " + response?.error,
+          toastOptions
+        );
+      }
+      setIsTraining(false);
     }
-    setIsTraining(false);
+  };
+
+  const handleFetchNextPage = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    fetchNextPage();
   };
 
   return (
-    <div className="flex flex-col items-start justify-center w-full">
-      <DataTable data={canonicalPageMap} columns={columns} />
+    <div className="flex flex-col items-center justify-center w-full">
+      {canonicalPageMap ? (
+        <DataTable data={canonicalPageMap} columns={columns} />
+      ) : null}
       <div className="flex w-full flex-col items-start justify-center gap-y-3">
         <div className="flex items-center justify-between w-full">
           <h3>Knowledge Bases</h3>
@@ -65,11 +86,22 @@ const SiteAIPages = ({ siteId }: { siteId: string }) => {
           </Button>
         </div>
         <ul className="grid grid-cols-2 gap-3 overflow-auto w-full">
-          {documents.map((document, index) => (
+          {documents?.map((document, index) => (
             <KnowledgeBaseCard key={index} document={document} />
           ))}
         </ul>
       </div>
+      <Button
+        className="rounded-xl mt-2 bg-green-600/60 hover:bg-green-600/80"
+        onClick={handleFetchNextPage}
+        disabled={!hasNextPage || isFetchingNextPage}
+      >
+        {isFetchingNextPage
+          ? "Loading ..."
+          : hasNextPage
+          ? "Show More"
+          : "Nothing more to show"}
+      </Button>
     </div>
   );
 };
