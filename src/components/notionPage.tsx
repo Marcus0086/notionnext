fb4a6ca420ee9221db554d3741887929a3e427d8
Marcus0086 <guptamarcus42@gmail.com>
2024-03-30 00:29:59 +0530

    Fix API route and Notion page rendering

--- src/components/notionPage.tsx
+++ src/components/notionPage.tsx
@@ -64,7 +64,7 @@ const NotionPage: React.FC<PageProps> = ({
           isLive,
         }),
     }),
-    [accountType, config, isLive, recordMap, site]
+    [accountType, config, isLive, recordMap, site],
   );
 
   const siteMapPageUrl = useMemo(() => {
@@ -182,7 +182,7 @@ const NotionPage: React.FC<PageProps> = ({
       {recordMap ? (
         <NotionRenderer
           bodyClassName={cn(
-            pageId === site?.rootNotionPageId ? "index-page" : ""
+            pageId === site?.rootNotionPageId ? "index-page" : "",
           )}
           showTableOfContents={isBlogPost}
           components={components}
