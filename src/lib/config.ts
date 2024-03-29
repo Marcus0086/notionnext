01a7332e2574428ce208b63a733a1b8694426b46
Marcus0086 <guptamarcus42@gmail.com>
2024-03-30 00:49:09 +0530

    Update domain and http prefix in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,8 @@
 const uuid = process.env.NODE_ENV === "development" ? true : false;
 
 const domainSuffix =
-  process.env.NODE_ENV === "development" ? "localhost:3000" : "localhost:3000";
+  process.env.NODE_ENV === "development" ? "localhost:3000" : "guptaraghav.org";
 const httpPrefix =
-  process.env.NODE_ENV === "development" ? "http://" : "http://";
+  process.env.NODE_ENV === "development" ? "http://" : "https://";
 
 export { uuid, domainSuffix, httpPrefix };
