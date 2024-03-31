d7984898e2abe8a485c17a5c8e82eef7ace60b93
Marcus0086 <guptamarcus42@gmail.com>
2024-04-01 00:36:32 +0530

    Update colorPickerDropdown component

--- src/components/dashboard/colorPickerDropdown.tsx
+++ src/components/dashboard/colorPickerDropdown.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Disclosure } from "@headlessui/react";
 import { FiChevronUp } from "react-icons/fi";
 import { HexColorPicker } from "react-colorful";
@@ -18,22 +18,31 @@ const ColorPickerDropdown = ({
 }: DesignSettingsDropDownType) => {
   const popover = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState<boolean[]>(
-    new Array(componentItems.length).fill(false),
+    new Array(componentItems.length).fill(false)
   );
   const [styleMap, setStyleMap] = useState<{
     [key: string]: {
       [key: string]: string | undefined;
     };
   }>(() => ({
-    main_background: {
+    main_bg: {
       css_var: "--bg-color",
+      css: "notion-page-scroller",
     },
-    main_text: {
+    main_text_color: {
       css_var: "--fg-color",
+      css: "notion-page",
+    },
+    navbar_bg: {
+      css_var: "--bg-color",
+      css: "notion-header",
+    },
+    navbar_text_color: {
+      css_var: "--fg-color",
+      css: "notion-header .breadcrumb",
     },
   }));
 
-  const { theme } = useTheme();
   const { settings, setSettings } = useParentPageSettings();
 
   const toggle = (index: number) => {
@@ -49,43 +58,67 @@ const ColorPickerDropdown = ({
       }
       setIsOpen(new Array(componentItems.length).fill(false));
     },
-    [componentItems.length],
+    [componentItems.length]
   );
   useClickOutside(popover, close);
 
   const handleColorChange = (color: string, id: string) => {
     const style = styleMap[id];
-    let css = "";
-    let cssClass = "";
-    const settingsCss = settings?.site?.css ?? "";
-    if (id === "main_background") {
-      cssClass = "notion-page-scroller";
-      const newColor = `background:${color}!important`;
-      css = `.notion-page-scroller {\n  ${newColor}\n}`;
-      setStyleMap({
-        ...styleMap,
-        [id]: {
-          ...style,
-          css_var: undefined,
-          css: cssClass,
-        },
-      });
-      setSettings({
-        ...settings,
-        site: {
-          ...settings?.site,
-          userId: settings?.site?.userId || "",
-          notionAuthToken: settings?.site?.notionAuthToken || "",
-          notionUserId: settings?.site?.notionUserId || "",
-          id: settings?.site?.id || "",
-          css: settingsCss + css,
-          name: settings?.site?.name || "",
-          rootNotionPageId: settings?.site?.rootNotionPageId || "",
-          rootNotionSpaceId: settings?.site?.rootNotionSpaceId || "",
-        },
-      });
-    }
+    if (!style) return;
+    setStyleMap((prev) => ({
+      ...prev,
+      [id]: { ...prev[id], color },
+    }));
+    setSettings({
+      ...settings,
+      config: {
+        ...settings?.config,
+        id: settings?.config?.id || "",
+        rootNotionPageId: settings?.config?.rootNotionPageId || "",
+        rootNotionSpaceId: settings?.config?.rootNotionSpaceId || "",
+        name: settings?.config?.name || "",
+        domain: settings?.config?.domain || "",
+        author: settings?.config?.author || "",
+        [id]: color,
+      },
+      miscelanous: {
+        ...settings?.miscelanous,
+        [id]: color,
+      },
+    });
   };
+
+  useEffect(() => {
+    if (settings) {
+      setStyleMap((prev) => ({
+        ...prev,
+        main_bg: {
+          ...prev.main_bg,
+          color: settings.config?.["main_bg"],
+        },
+        main_text_color: {
+          ...prev.main_text_color,
+          color: settings.config?.["main_text_color"],
+        },
+        navbar_bg: {
+          ...prev.navbar_bg,
+          color: settings.config?.["navbar_bg"],
+        },
+        navbar_text_color: {
+          ...prev.navbar_text_color,
+          color: settings.config?.["navbar_text_color"],
+        },
+        footer_bg: {
+          ...prev.footer_bg,
+          color: settings.config?.["footer_bg"],
+        },
+        footer_text_color: {
+          ...prev.footer_text_color,
+          color: settings.config?.["footer_text_color"],
+        },
+      }));
+    }
+  }, [settings]);
 
   return (
     <Disclosure defaultOpen={false}>
@@ -97,38 +130,37 @@ const ColorPickerDropdown = ({
               "text-base font-medium text-cloudBurst dark:text-selago",
               "w-full flex justify-between items-start outline-none cursor-pointer p-2 rounded-lg",
               "border border-gray-300 dark:border-navy-700",
-              "bg-white dark:bg-navy-800",
+              "bg-white dark:bg-navy-800"
             )}
           >
             {title}
             <FiChevronUp
               className={cn(
                 "w-5 h-5 transition-transform duration-150 ease-in-out",
-                open ? "transform rotate-180" : "",
+                open ? "transform rotate-180" : ""
               )}
             />
           </Disclosure.Button>
           <Disclosure.Panel
             className={cn(
-              "flex flex-col items-center justify-center gap-y-6 p-4 bg-white dark:bg-navy-800 rounded-md",
+              "flex flex-col items-center justify-center gap-y-6 p-4 bg-white dark:bg-navy-800 rounded-md"
             )}
           >
             {componentItems.map((item, index) => (
               <div
                 key={index}
                 className={cn(
-                  "flex items-center justify-between gap-x-2 w-full ",
+                  "flex items-center justify-between gap-x-2 w-full "
                 )}
               >
                 <span className="text-sm">{item.name}</span>
                 <button
                   className={cn(
-                    "relative border border-gray-300 !p-0 !w-4 !h-4 !rounded-full !cursor-pointer !min-h-4  !flex-grow-0",
-                    styleMap[item.id]?.["css"] ? styleMap[item.id]["css"] : "",
+                    "relative border border-gray-300 !p-0 !w-4 !h-4 !rounded-full !cursor-pointer !min-h-4  !flex-grow-0"
                   )}
                   style={{
-                    background: styleMap[item.id]?.["css"]
-                      ? ""
+                    background: styleMap[item.id]?.["color"]
+                      ? styleMap[item.id]?.["color"]
                       : `var(${styleMap[item.id]?.["css_var"]})` || item.color,
                   }}
                   onClick={() => toggle(index)}
@@ -143,6 +175,7 @@ const ColorPickerDropdown = ({
                         onChange={(newColor) =>
                           handleColorChange(newColor, item.id)
                         }
+                        color={styleMap[item.id]?.["color"] || item.color}
                         onClick={(e) => e.stopPropagation()}
                       />
                     </div>
