d9acdfe0b04e732a48ef87ff32b5c142793f2953
Marcus0086 <guptamarcus42@gmail.com>
2024-04-03 14:47:36 +0530

    Update form styling in CreateSiteForm component

--- src/components/dashboard/form.tsx
+++ src/components/dashboard/form.tsx
@@ -11,7 +11,7 @@ const CreateSiteForm = () => {
   return (
     <form
       action={action}
-      className="flex flex-col items-start justify-center py-10 px-8 md:py-12 md:px-16 font-normal text-white antialiased"
+      className="flex flex-col items-start justify-center py-10 px-8 md:py-12 md:px-16 font-normal antialiased"
     >
       <BackgroundBeams />
       {/* TODO: Ask chatgpt for a better copy text */}
@@ -32,7 +32,7 @@ const CreateSiteForm = () => {
           id="sitename"
           placeholder="notionnext"
           required
-          className="outline-none text-cloudBurst dark:text-white rounded-xl py-2 px-4 w-full sm:w-2/4"
+          className="outline-none text-cloudBurst dark:text-white rounded-xl py-2 px-4 w-full sm:w-2/4 bg-selago dark:bg-blueZodiac/40"
         />
         <label htmlFor="url" className="m-1 mt-4 text-xs">
           URL
@@ -43,7 +43,7 @@ const CreateSiteForm = () => {
           id="url"
           placeholder="https://example.notion.site/xyz"
           required
-          className="w-full outline-none text-cloudBurst dark:text-white rounded-xl py-2 px-4"
+          className="w-full outline-none text-cloudBurst dark:text-white rounded-xl py-2 px-4 bg-selago dark:bg-blueZodiac/40"
         />
         <FormSubmitButton>Create</FormSubmitButton>
       </div>
