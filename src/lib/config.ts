11c88c1577abac59f3a1a39603bc1f1413d54629
Marcus0086 <guptamarcus42@gmail.com>
2024-04-08 00:50:04 +0530

    Update domainSuffix variable in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,9 @@
 const uuid = process.env.NODE_ENV === "development" ? true : false;
 
 const domainSuffix =
-  process.env.NODE_ENV === "development" ? "localhost:3000" : "guptaraghav.org";
+  process.env.NODE_ENV === "development"
+    ? "localhost:3000"
+    : process.env.NEXT_PUBLIC_ROOT_DOMAIN;
 const httpPrefix =
   process.env.NODE_ENV === "development" ? "http://" : "https://";
 
