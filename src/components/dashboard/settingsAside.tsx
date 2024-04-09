eab75b7c7b7bb46e1ff946c089909b2cff92bdae
Marcus0086 <guptamarcus42@gmail.com>
2024-04-10 02:53:12 +0530

    Update settingsAside.tsx component with new dependencies and styles

--- src/components/dashboard/settingsAside.tsx
+++ src/components/dashboard/settingsAside.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Link from "next/link";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 import ThemeButton from "../shared/themeButton";
 import AisdeButtons from "@/components/dashboard/asideButtons";
 import LoadingComponent from "@/components/dashboard/loadingComponent";
+import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
 
@@ -16,41 +20,56 @@ const SettingsAside = ({
   items: AsideMenuType[];
   siteId?: string;
 }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <aside
-      className={cn(
-        "flex flex-col min-h-full",
-        "bg-white dark:bg-navy-800 drop-shadow-sm",
-        "sm:translate-x-0",
-        "transition-all duration-150 ease-linear -translate-x-96",
-        "fixed z-50 ",
-      )}
-    >
-      <section className="w-20 h-20 flex items-center justify-center">
-        <Link href="/home">
-          <h1 className="text-center text-2xl font-semibold uppercase text-cloudBurst dark:text-white italic">
-            NS
-          </h1>
-        </Link>
-      </section>
-      <hr className="bg-gray-400 w-full" />
-      <Suspense fallback={<LoadingComponent />}>
-        <ul className="flex flex-col">
-          {items.map(({ title, icon, link }, index) => (
-            <AisdeButtons
-              key={index}
-              siteId={siteId || ""}
-              link={link}
-              icon={icon}
-              title={title}
-            />
-          ))}
-        </ul>
-      </Suspense>
-      <div className="flex flex-col items-center justify-center py-4 mx-auto mt-auto">
-        <ThemeButton />
-      </div>
-    </aside>
+    <>
+      <Button
+        className="sm:hidden absolute right-32 top-4 z-20 flex items-center justify-center"
+        variant="outline"
+        onClick={toggleMenu}
+      >
+        <HamburgerMenuIcon className="w-6 h-6" />
+      </Button>
+
+      <aside
+        className={cn(
+          "flex flex-col min-h-full",
+          "bg-white dark:bg-navy-800 drop-shadow-sm",
+          isOpen ? "block sm:block" : "hidden sm:block",
+          "transition-all duration-150 ease-linear",
+          "fixed z-50 "
+        )}
+      >
+        <section className="w-20 h-20 flex items-center justify-center">
+          <Link href="/home">
+            <h1 className="text-center text-2xl font-semibold uppercase text-cloudBurst dark:text-white italic">
+              NS
+            </h1>
+          </Link>
+        </section>
+        <hr className="bg-gray-400 w-full" />
+        <Suspense fallback={<LoadingComponent />}>
+          <ul className="flex flex-col">
+            {items.map(({ title, icon, link }, index) => (
+              <AisdeButtons
+                key={index}
+                siteId={siteId || ""}
+                link={link}
+                icon={icon}
+                title={title}
+              />
+            ))}
+          </ul>
+        </Suspense>
+        <div className="flex flex-col items-center justify-center py-4 mx-auto mt-auto">
+          <ThemeButton />
+        </div>
+      </aside>
+    </>
   );
 };
 
