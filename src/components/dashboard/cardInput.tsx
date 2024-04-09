d2948d592f46fa6050223b5982580600877c6507
Marcus0086 <guptamarcus42@gmail.com>
2024-04-10 02:52:49 +0530

    Add customDomain prop to CardInput component

--- src/components/dashboard/cardInput.tsx
+++ src/components/dashboard/cardInput.tsx
@@ -12,6 +12,7 @@ const CardInput = ({
   siteId,
   title,
   description,
+  customDomain,
 }: {
   type: CardInputs;
   name?: string;
@@ -21,23 +22,26 @@ const CardInput = ({
   siteId?: string;
   title?: string;
   description?: string;
+  customDomain?: string;
 }) => {
   const value =
     type === "text"
       ? name
       : type === "url"
-        ? !url
-          ? ""
-          : `https://notion.so/${url}`
-        : type === "font"
-          ? font
-          : type === "visibility"
-            ? visibility
-            : type === "opentext"
-              ? title
-              : type === "textarea"
-                ? description
-                : siteId;
+      ? !url
+        ? ""
+        : `https://notion.so/${url}`
+      : type === "font"
+      ? font
+      : type === "visibility"
+      ? visibility
+      : type === "opentext"
+      ? title
+      : type === "textarea"
+      ? description
+      : type === "listadd"
+      ? customDomain
+      : siteId;
   const Input = CardsInputFactory.getInput(type, value);
   return Input;
 };
