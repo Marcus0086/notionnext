b025cf9fbea999a00ed8ff9c2eecb77e4ea19430
Marcus0086 <guptamarcus42@gmail.com>
2024-04-10 02:54:08 +0530

    Add inputs.ts file to handle card and toggle inputs in src/lib/inputs.ts

---
+++ src/lib/inputs.ts
@@ -0,0 +1,29 @@
+import { CardInputs, ToggleInputs } from "@/types/component-types";
+
+const isCardInput = (type: string): type is CardInputs => {
+  return [
+    "text",
+    "opentext",
+    "textarea",
+    "font",
+    "media",
+    "url",
+    "visibility",
+    "delete",
+  ].includes(type);
+};
+
+const isToggleInput = (type: string): type is ToggleInputs => {
+  return [
+    "theme",
+    "search",
+    "ai",
+    "preview",
+    "twitter",
+    "toploader",
+    "prettyurls",
+    "sitemap",
+  ].includes(type);
+};
+
+export { isCardInput, isToggleInput };
