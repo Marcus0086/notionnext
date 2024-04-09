4421909e80557204639eed521a3e72bba61da8e2
Marcus0086 <guptamarcus42@gmail.com>
2024-04-10 02:54:13 +0530

    Refactor site actions imports in src/lib/actions/site/index.ts

--- src/lib/actions/site/index.ts
+++ src/lib/actions/site/index.ts
@@ -1,25 +1,11 @@
-import { createSite } from "./create";
-import { getSiteById, siteImage, sitePage } from "@/lib/actions/site/get";
-import { deleteSiteById } from "@/lib/actions/site/delete";
-import { saveSiteData } from "@/lib/actions/site/save";
-import { getSiteDocuments } from "@/lib/actions/site/get/ai";
-import { getFilesFromRedis } from "@/lib/actions/site/get";
-import { getUserSites } from "@/lib/actions/site/get/users";
-import { getDesignSiteCardById } from "@/lib/actions/site/get/design";
-import { getGeneralSiteCardById } from "@/lib/actions/site/get/general";
-import { getOptionsSiteCardById } from "@/lib/actions/site/get/options";
-
-export {
-  createSite,
-  getUserSites,
-  deleteSiteById,
-  getGeneralSiteCardById,
-  getDesignSiteCardById,
-  getSiteById,
-  sitePage,
-  saveSiteData,
-  siteImage,
-  getOptionsSiteCardById,
-  getSiteDocuments,
-  getFilesFromRedis,
-};
+export { createSite } from "@/lib/actions/site/create";
+export { getSiteById, siteImage, sitePage } from "@/lib/actions/site/get";
+export { deleteSiteById } from "@/lib/actions/site/delete";
+export { saveSiteData } from "@/lib/actions/site/save";
+export { getSiteDocuments } from "@/lib/actions/site/get/ai";
+export { getFilesFromRedis } from "@/lib/actions/site/get";
+export { getUserSites } from "@/lib/actions/site/get/users";
+export { getDesignSiteCardById } from "@/lib/actions/site/get/design";
+export { getGeneralSiteCardById } from "@/lib/actions/site/get/general";
+export { getOptionsSiteCardById } from "@/lib/actions/site/get/options";
+export { getDomainsSiteCardById } from "@/lib//actions/site/get/domains";
