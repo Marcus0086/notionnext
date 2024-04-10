d03d5a3cc0f51fccabf6ba09447c9dc5475f6c88
Marcus0086 <guptamarcus42@gmail.com>
2024-04-11 01:35:42 +0530

    Refactor asideButtons component in dashboard/asideButtons.tsx to improve button styling and add disableName prop

--- src/components/dashboard/asideButtons.tsx
+++ src/components/dashboard/asideButtons.tsx
@@ -14,11 +14,13 @@ const AisdeButtons = ({
   link,
   icon,
   title,
+  disableName,
 }: {
   siteId: string;
   link: string;
   icon: Icons;
   title: string;
+  disableName?: boolean;
 }) => {
   const pathName = usePathname();
   const searchParams = useSearchParams();
@@ -38,15 +40,15 @@ const AisdeButtons = ({
       <li
         className={cn(
           "flex flex-col gap-y-2 items-center justify-center relative",
-          "w-20 h-20 rounded-lg",
+          "w-14 h-14 sm:w-20 sm:h-20 rounded-lg",
           "hover:bg-blueZodiac/20 dark:hover:bg-navy-900 transition duration-150 ease-in",
           fullPathName === href
-            ? "after:content-[''] after:w-1 after:h-8 after:right-0 after:bg-brandLinear after:rounded-l-md after:absolute"
+            ? "after:content-[''] sm:after:w-1 sm:after:h-8 after:w-full after:h-1 after:bottom-0 sm:after:bottom-auto sm:after:right-0 after:bg-brandLinear sm:after:!rounded-l-md after:rounded-b-md sm:after:rounded-b-none after:absolute"
             : ""
         )}
       >
         <AsideIcons icon={icon} label={title} />
-        <h3 className="text-xs">{title}</h3>
+        {disableName ? null : <h3 className="text-xs">{title}</h3>}
       </li>
     </Link>
   );
