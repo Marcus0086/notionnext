9752939f1d275cdce69ab085d5ee5dcca3f5125a
Marcus0086 <guptamarcus42@gmail.com>
2024-04-11 01:36:17 +0530

    Refactor SettingsAside component in settingsAside.tsx to remove unused imports and state

--- src/components/dashboard/settingsAside.tsx
+++ src/components/dashboard/settingsAside.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import Link from "next/link";
-import { Suspense, useState } from "react";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Suspense } from "react";
 
 import ThemeButton from "../shared/themeButton";
 import AisdeButtons from "@/components/dashboard/asideButtons";
 import LoadingComponent from "@/components/dashboard/loadingComponent";
-import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
 
@@ -20,26 +16,37 @@ const SettingsAside = ({
   items: AsideMenuType[];
   siteId?: string;
 }) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <>
-      <Button
-        className="sm:hidden absolute right-32 top-4 z-20 flex items-center justify-center"
-        variant="outline"
-        onClick={toggleMenu}
-      >
-        <HamburgerMenuIcon className="w-6 h-6" />
-      </Button>
-
+      <aside className="flex items-center justify-center gap-2 dark:bg-navy-800 sm:hidden shadow-2xl">
+        <section className="w-14 h-14 sm:w-20 sm:h-20 flex items-center justify-center">
+          <Link href="/home">
+            <h1 className="text-center text-2xl font-semibold uppercase text-cloudBurst dark:text-white italic">
+              NS
+            </h1>
+          </Link>
+        </section>
+        <hr className="bg-gray-400 w-[1px] h-14" />
+        <ul className="h-auto w-full overflow-auto flex items-center justify-start gap-x-4 no-scroll">
+          {items.map(({ title, icon, link }, index) => (
+            <AisdeButtons
+              key={index}
+              siteId={siteId || ""}
+              link={link}
+              icon={icon}
+              title={title}
+              disableName
+            />
+          ))}
+        </ul>
+        <div className="flex flex-col items-center justify-center py-4 mx-auto mt-auto">
+          <ThemeButton />
+        </div>
+      </aside>
       <aside
         className={cn(
-          "flex flex-col min-h-full",
           "bg-white dark:bg-navy-800 drop-shadow-sm",
-          isOpen ? "block sm:block" : "hidden sm:block",
+          "hidden sm:flex flex-col min-h-full",
           "transition-all duration-150 ease-linear",
           "fixed z-50 "
         )}
