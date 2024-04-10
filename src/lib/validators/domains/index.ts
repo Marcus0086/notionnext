ff7ccd188b7c6364fbf48379e6c9748d494bdea0
Marcus0086 <guptamarcus42@gmail.com>
2024-04-11 01:36:12 +0530

    Add DNS validation utility function and custom domain validation in domains/index.ts

---
+++ src/lib/validators/domains/index.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+
+import { RecordType } from "@/lib/validators/domains/dns";
+
+const subdomainSchema = z.string().refine(
+  async (value) => {
+    if (value.length <= 1 || value.length > 63) return false;
+
+    const subdomainRegex = /^[a-z0-9-]+$/;
+    if (!subdomainRegex.test(value)) return false;
+
+    if (value.startsWith("-") || value.endsWith("-")) return false;
+
+    return true;
+  },
+  {
+    message: "Cannot create site with invalid subdomain name",
+  }
+);
+
+const customDomainSchema = z.string().refine(
+  async (value) => {
+    if (value.length <= 1 || value.length > 253) return false;
+    const customDomainRegex = /^[a-z0-9]+(-[a-z0-9]+)*\.[a-z]{2,}$/;
+    return customDomainRegex.test(value);
+  },
+  {
+    message: "Cannot add invalid custom domain name",
+  }
+);
+
+const isApexDomain = (
+  domain: string
+):
+  | {
+      recordType: RecordType;
+      expectedValue: string;
+    }
+  | undefined => {
+  const dotCount = domain.split(".").length - 1;
+  if (dotCount === 1) {
+    return {
+      recordType: "A",
+      expectedValue: process.env.SERVER_IP as string,
+    };
+  }
+};
+export { subdomainSchema, customDomainSchema, isApexDomain };
