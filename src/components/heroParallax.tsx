e9a0d517466cae9753c55a2d2857e4a14a403226
Marcus0086 <guptamarcus42@gmail.com>
2024-04-12 01:45:21 +0530

    Refactor code to update styles and fix bugs

--- src/components/heroParallax.tsx
+++ src/components/heroParallax.tsx
@@ -58,7 +58,7 @@ const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[265vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[265vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d] bg-neutral-950"
     >
       <Header />
       <motion.div
@@ -105,10 +105,10 @@ const HeroParallax = ({
 export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
+      <h1 className="text-2xl md:text-7xl font-bold text-white">
         The Ultimate <br /> development studio
       </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+      <p className="max-w-2xl text-base md:text-xl mt-8 text-neutral-200">
         We build beautiful products with the latest technologies and frameworks.
         We are a team of passionate developers and designers that love to build
         amazing products.
