11485feb9cca35e04fe3e06b4a84b923019438c2
Marcus0086 <guptamarcus42@gmail.com>
2024-04-12 01:46:10 +0530

    Update themeButton component styles and add forceVisible prop

--- src/components/shared/themeButton.tsx
+++ src/components/shared/themeButton.tsx
@@ -1,10 +1,13 @@
 "use client";
+
 import { useTheme } from "next-themes";
 import { RxMoon, RxSun } from "react-icons/rx";
 
 import { Button } from "@/components/ui/button";
 
-const ThemeButton = () => {
+import { cn } from "@/lib/utils";
+
+const ThemeButton = ({ forceVisible }: { forceVisible?: boolean }) => {
   const { theme, setTheme } = useTheme();
 
   const handleClick = () => {
@@ -18,9 +21,20 @@ const ThemeButton = () => {
       size="icon"
       className="focus:!outline-none focus:!ring-transparent hover:!bg-transparent"
     >
-      <RxSun className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <RxMoon className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
+      <RxSun
+        className={cn(
+          "h-[1rem] w-[1rem]",
+          forceVisible ? "text-white" : "",
+          "rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0",
+        )}
+      />
+      <RxMoon
+        className={cn(
+          "absolute",
+          "h-[1rem] w-[1rem]",
+          "rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100",
+        )}
+      />
     </Button>
   );
 };
