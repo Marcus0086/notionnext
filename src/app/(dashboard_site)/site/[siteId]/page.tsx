2e916c4c396d56e3f73598a19cd775c8988c8c00
Marcus0086 <guptamarcus42@gmail.com>
2024-04-14 02:11:10 +0530

    Add Suspense component to SiteSettingsPage in src/app/(dashboard_site)/site/[siteId]/page.tsx

--- src/app/(dashboard_site)/site/[siteId]/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/page.tsx
@@ -1,14 +1,15 @@
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
+import LoadingCard from "@/components/dashboard/loadingCard";
+import { GENERAL_SETTINGS } from "@/components/dashboard/constants";
 const SiteCard = dynamic(() => import("@/components/dashboard/siteCard"));
 const NameInputCard = dynamic(
-  () => import("@/components/dashboard/nameInputCard"),
+  () => import("@/components/dashboard/nameInputCard")
 );
-import { GENERAL_SETTINGS } from "@/components/dashboard/constants";
 
 import { getGeneralSiteCardById } from "@/lib/actions/site";
-
 import { SitePageParams } from "@/types";
 
 const SiteSettingsPage = async ({ params: { siteId } }: SitePageParams) => {
@@ -18,18 +19,22 @@ const SiteSettingsPage = async ({ params: { siteId } }: SitePageParams) => {
   }
   return (
     <>
-      <SiteCard {...siteCard} delete={false} settings={true} />
-      {GENERAL_SETTINGS.map((card) => (
-        <NameInputCard
-          {...card}
-          {...siteCard}
-          url={siteCard.siteConfig?.rootNotionPageId || ""}
-          type={card.type}
-          siteId={siteId}
-          key={card.title}
-          siteTitle={siteCard?.name}
-        />
-      ))}
+      <Suspense fallback={<LoadingCard />}>
+        <SiteCard {...siteCard} delete={false} settings={true} />
+      </Suspense>
+      <Suspense fallback={<LoadingCard />}>
+        {GENERAL_SETTINGS.map((card) => (
+          <NameInputCard
+            {...card}
+            {...siteCard}
+            url={siteCard.siteConfig?.rootNotionPageId || ""}
+            type={card.type}
+            siteId={siteId}
+            key={card.title}
+            siteTitle={siteCard?.name}
+          />
+        ))}
+      </Suspense>
     </>
   );
 };
