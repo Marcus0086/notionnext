452afb1e2999c5a01d0f7437fd26f659f199242f
Marcus0086 <guptamarcus42@gmail.com>
2024-04-13 16:58:33 +0530

    Add HeroParallax component to HomePage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,21 @@
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+import { DEMOS } from "@/components/dashboard/constants";
+import LoadingCard from "@/components/dashboard/loadingCard";
 import Hero from "@/components/hero";
 import Pricing from "@/components/pricing";
+const HeroParallax = dynamic(() => import("@/components/heroParallax"));
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <section>
+        <Suspense fallback={<LoadingCard />}>
+          <HeroParallax products={DEMOS} />
+        </Suspense>
+      </section>
       <Pricing />
     </>
   );
