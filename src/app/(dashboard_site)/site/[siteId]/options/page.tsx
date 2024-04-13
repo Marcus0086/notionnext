35cfe3c9b17fadf88c451ee095511bc675f676d9
Marcus0086 <guptamarcus42@gmail.com>
2024-04-14 02:11:25 +0530

    Add Suspense component to OptionsPage in src/app/(dashboard_site)/site/[siteId]/options/page.tsx

--- src/app/(dashboard_site)/site/[siteId]/options/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/options/page.tsx
@@ -1,9 +1,11 @@
 import { notFound } from "next/navigation";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 import { OPTIONS_SETTINGS } from "@/components/dashboard/constants";
 const ToggleInput = dynamic(() => import("@/components/dashboard/toggleInput"));
+import LoadingCard from "@/components/dashboard/loadingCard";
 
 import getQueryClient from "@/context/queryClient";
 import { getOptionsSiteCardById } from "@/lib/actions/site";
@@ -23,9 +25,11 @@ const OptionsPage = async ({ params: { siteId } }: SitePageParams) => {
   const dehydratedState = dehydrate(queryClient);
   return (
     <HydrationBoundary state={dehydratedState}>
-      {OPTIONS_SETTINGS.map((card, index) => (
-        <ToggleInput key={index} {...card} siteId={siteId} />
-      ))}
+      <Suspense fallback={<LoadingCard />}>
+        {OPTIONS_SETTINGS.map((card, index) => (
+          <ToggleInput key={index} {...card} siteId={siteId} />
+        ))}
+      </Suspense>
     </HydrationBoundary>
   );
 };
