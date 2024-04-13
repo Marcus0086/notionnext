901749555536ca322a384d991f8330cb899ac43c
Marcus0086 <guptamarcus42@gmail.com>
2024-04-14 02:11:20 +0530

    Add Suspense component to DesignPage in src/app/(dashboard_site)/site/[siteId]/design/page.tsx

--- src/app/(dashboard_site)/site/[siteId]/design/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/design/page.tsx
@@ -1,19 +1,21 @@
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 import {
   DESIGN_SETTINGS,
   DESIGN_SETTINGS_DROP_DOWN,
 } from "@/components/dashboard/constants";
 const NameInputCard = dynamic(
-  () => import("@/components/dashboard/nameInputCard"),
+  () => import("@/components/dashboard/nameInputCard")
 );
 const ColorPaletteDropDown = dynamic(
-  () => import("@/components/dashboard/colorPaletteDropDown"),
+  () => import("@/components/dashboard/colorPaletteDropDown")
 );
 const ColorPickerDropdown = dynamic(
-  () => import("@/components/dashboard/colorPickerDropdown"),
+  () => import("@/components/dashboard/colorPickerDropdown")
 );
+import LoadingCard from "@/components/dashboard/loadingCard";
 
 import { getDesignSiteCardById } from "@/lib/actions/site";
 
@@ -26,23 +28,29 @@ const DesignPage = async ({ params: { siteId } }: SitePageParams) => {
 
   return (
     <>
-      <ColorPaletteDropDown />
-      {DESIGN_SETTINGS_DROP_DOWN.map(({ title, componentItems }, index) => (
-        <ColorPickerDropdown
-          title={title}
-          componentItems={componentItems}
-          key={index}
-        />
-      ))}
-      {DESIGN_SETTINGS.map((card) => (
-        <NameInputCard
-          {...card}
-          {...siteCard}
-          type={card.type}
-          siteId={siteId}
-          key={card.title}
-        />
-      ))}
+      <Suspense fallback={<LoadingCard />}>
+        <ColorPaletteDropDown />
+      </Suspense>
+      <Suspense fallback={<LoadingCard />}>
+        {DESIGN_SETTINGS_DROP_DOWN.map(({ title, componentItems }, index) => (
+          <ColorPickerDropdown
+            title={title}
+            componentItems={componentItems}
+            key={index}
+          />
+        ))}
+      </Suspense>
+      <Suspense fallback={<LoadingCard />}>
+        {DESIGN_SETTINGS.map((card) => (
+          <NameInputCard
+            {...card}
+            {...siteCard}
+            type={card.type}
+            siteId={siteId}
+            key={card.title}
+          />
+        ))}
+      </Suspense>
     </>
   );
 };
