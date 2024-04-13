fea30a2dfe057d142a7c44b6e19979fb650ad958
Marcus0086 <guptamarcus42@gmail.com>
2024-04-14 02:11:15 +0530

    Add Suspense component to SiteSettingsPage in src/app/(dashboard_site)/site/[siteId]/page.tsx

--- src/app/(dashboard_site)/site/[siteId]/code/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/code/page.tsx
@@ -1,13 +1,17 @@
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 const Editor = dynamic(() => import("@/components/dashboard/editor"));
 import UpgradePlanCard from "@/components/dashboard/upgradePlanCard";
+import LoadingCard from "@/components/dashboard/loadingCard";
 
 const CodeSettings = () => {
   return (
     <>
       <UpgradePlanCard type={"code"} />
-      <Editor />
+      <Suspense fallback={<LoadingCard />}>
+        <Editor />
+      </Suspense>
     </>
   );
 };
