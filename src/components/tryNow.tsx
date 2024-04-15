b563070a8e5a3a5e23c76d613de2586446659f0e
Marcus0086 <guptamarcus42@gmail.com>
2024-04-16 01:20:12 +0530

    Update TryNowButton component styles in tryNow.tsx

--- src/components/tryNow.tsx
+++ src/components/tryNow.tsx
@@ -18,7 +18,7 @@ const TryNowButton = (props: Props) => {
           "md:p-8 p-6 mb-8 md:mb-0 text-xl md:text-2xl gap-4",
           "w-fit",
           "border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white",
-          "group transition-all hover:shadow-xl hover:shadow-neutral-500 duration-500"
+          "group transition-all hover:shadow-xl hover:shadow-neutral-500 duration-500",
         )}
       >
         <span
@@ -26,7 +26,7 @@ const TryNowButton = (props: Props) => {
             "bg-clip-text text-transparent",
             "bg-gradient-to-r from-neutral-500  to-neutral-600",
             "md:text-center font-sans",
-            "group-hover:bg-gradient-to-r group-hover::from-black group-hover:to-black"
+            "group-hover:bg-gradient-to-r group-hover::from-black group-hover:to-black",
           )}
         >
           Try Now
