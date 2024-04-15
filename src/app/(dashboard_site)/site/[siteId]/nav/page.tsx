e7151d50ced1effa33d1747e84d81a916e0c508b
Marcus0086 <guptamarcus42@gmail.com>
2024-04-16 01:19:35 +0530

    Update NavSettings page to fetch and display navigation cards

--- src/app/(dashboard_site)/site/[siteId]/nav/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/nav/page.tsx
@@ -1,7 +1,37 @@
+import { Suspense } from "react";
+import { notFound } from "next/navigation";
+
+import { NAV_SETTINGS } from "@/components/dashboard/constants";
+import LoadingCard from "@/components/dashboard/loadingCard";
+import NameInputCard from "@/components/dashboard/nameInputCard";
+
+import { getNavSiteCardById } from "@/lib/actions/site/get/nav";
+
 import { SitePageParams } from "@/types/site";
 
 const NavSettings = async ({ params: { siteId } }: SitePageParams) => {
-  return <div>Nav</div>;
+  const navSiteCard = await getNavSiteCardById(siteId);
+  if (!navSiteCard || !navSiteCard.siteConfig) {
+    notFound();
+  }
+  return (
+    <>
+      {NAV_SETTINGS.map((card, index) => (
+        <Suspense key={card.title} fallback={<LoadingCard />}>
+          <NameInputCard
+            key={index}
+            {...card}
+            {...navSiteCard}
+            siteId={siteId}
+            navigationStyle={
+              navSiteCard.siteConfig?.navigationStyle || "default"
+            }
+            type={card.type}
+          />
+        </Suspense>
+      ))}
+    </>
+  );
 };
 
 export default NavSettings;
