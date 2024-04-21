8688998008b798a07cf37840391d0f813187529b
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:43:06 +0530

    Refactor getCanonicalPageId function to remove unnecessary default value assignment in parameter

--- src/lib/getCanonicalPageId.ts
+++ src/lib/getCanonicalPageId.ts
@@ -7,7 +7,7 @@ import {
 export function getCanonicalPageId(
   pageId: string,
   recordMap: ExtendedRecordMap,
-  { uuid = true }: { uuid?: boolean } = {},
+  { uuid = true }: { uuid?: boolean } = {}
 ): string | null {
   const cleanPageId = parsePageId(pageId, { uuid: false });
   if (!cleanPageId) {
