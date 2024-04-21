ae6e0d37c0c43611426efe06313aa5cde16b80c9
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:42:35 +0530

    Refactor SiteAIPages component to remove unused imports and simplify rendering

--- src/components/dashboard/siteAIPages.tsx
+++ src/components/dashboard/siteAIPages.tsx
@@ -4,33 +4,21 @@ import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
 
 const KnowledgeBases = dynamic(
-  () => import("@/components/dashboard/knowledgeBases"),
+  () => import("@/components/dashboard/knowledgeBases")
 );
-import { DataTable } from "@/components/dashboard/dataTable";
-import { PageLinks, columns } from "@/components/dashboard/column";
 import LoadingCard from "@/components/dashboard/loadingCard";
 
-import { getSiteDocuments } from "@/lib/actions/site";
 import getQueryClient from "@/context/queryClient";
 
-import { SiteMap } from "@/types";
+import { getSiteDocuments } from "@/lib/actions/site";
 
 const SiteAIPages = async ({
-  siteMap,
   siteId,
   page,
 }: {
-  siteMap: SiteMap;
   siteId: string;
   page: number;
 }) => {
-  const canonicalPageMap = Object.entries(siteMap.canonicalPageMap);
-  const pageLinks = canonicalPageMap.map(
-    ([key], index) =>
-      ({
-        path: index === 0 ? "/home" : key,
-      }) as PageLinks,
-  );
   const queryClient = getQueryClient();
   try {
     await queryClient.prefetchQuery({
@@ -43,15 +31,11 @@ const SiteAIPages = async ({
   }
   const dehydratedState = dehydrate(queryClient);
   return (
-    <div className="flex flex-col items-start justify-center w-full gap-y-4">
-      <h3>Pages</h3>
-      <DataTable data={pageLinks} columns={columns} />
-      <HydrationBoundary state={dehydratedState}>
-        <Suspense fallback={<LoadingCard />}>
-          <KnowledgeBases siteId={siteId} page={page} />
-        </Suspense>
-      </HydrationBoundary>
-    </div>
+    <HydrationBoundary state={dehydratedState}>
+      <Suspense fallback={<LoadingCard />}>
+        <KnowledgeBases siteId={siteId} page={page} />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
