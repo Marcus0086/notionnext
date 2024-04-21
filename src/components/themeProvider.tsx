ce763c0da8769ccb87e0d8eefb5c7f59f65ed2c2
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:42:06 +0530

    Update ThemeProvider component to disable console logs in production

--- src/components/themeProvider.tsx
+++ src/components/themeProvider.tsx
@@ -4,7 +4,20 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
+const env = process.env.NODE_ENV || "development";
+
+const noConsole = () => {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+};
+
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  React.useEffect(() => {
+    if (env !== "development") {
+      noConsole();
+    }
+  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 };
 
