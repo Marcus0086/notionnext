dccfe9ef14e831a322f98446df1777a81c7cea13
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:43:26 +0530

    Refactor searchNotion.ts to use cache function and update imports

--- src/lib/searchNotion.ts
+++ src/lib/searchNotion.ts
@@ -1,37 +1,12 @@
-import ExpiryMap from "expiry-map";
+"use server";
+
+import { cache } from "react";
 import { SearchParams, SearchResults } from "notion-types";
-import pMemoize from "p-memoize";
+import { notionSearch } from "@/lib/getPage";
 
-export const apiBaseUrl = `/api`;
-
-export const api = {
-  searchNotion: `${apiBaseUrl}/search`,
-  getNotionPageInfo: `${apiBaseUrl}/notion-page-info`,
-  getSocialImage: `${apiBaseUrl}/social-image`,
-};
-
-const searchNotionImpl = async (
-  params: SearchParams,
-): Promise<SearchResults> => {
-  return fetch(api.searchNotion, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res;
-      }
-      const error: any = new Error(res.statusText);
-      error.response = res;
-      return Promise.reject(error);
-    })
-    .then((res) => res.json());
-};
-
-export const searchNotion = pMemoize(searchNotionImpl, {
-  cacheKey: (args) => args[0],
-  cache: new ExpiryMap(10000),
-});
+export const searchNotion = cache(
+  async (params: SearchParams): Promise<SearchResults> => {
+    const results = await notionSearch(params);
+    return results;
+  }
+);
