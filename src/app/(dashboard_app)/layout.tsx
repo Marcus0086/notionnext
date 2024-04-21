4db53c982dc5dc3982f722eec9cb2a79cef63745
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:36:27 +0530

    Update layout.tsx to fix CSS class name in RootLayout component

--- src/app/(dashboard_app)/layout.tsx
+++ src/app/(dashboard_app)/layout.tsx
@@ -13,7 +13,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex h-full w-full">
           <Aside items={ASIDE_MENU} />
           <section className="w-full h-full bg-lightPrimary dark:bg-navy-900">
-            <section className="mx-3 flex-none h-full transition-all md:pr-2 xl:ml-80">
+            <section className="mx-3 flex-none h-full transition-all md:pr-2 md:ml-80">
               <section className="h-full overflow-auto no-scroll">
                 <NavBar />
                 {children}
