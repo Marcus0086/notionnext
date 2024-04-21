1dde683f368db1c52bf098305eff3636c5157910
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:40:25 +0530

    Update page.tsx to remove unused code and simplify AIChatSettings function

--- src/app/(dashboard_site)/site/[siteId]/ai/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/ai/page.tsx
@@ -1,8 +1,4 @@
-import { notFound } from "next/navigation";
-
 import SiteAIPages from "@/components/dashboard/siteAIPages";
-
-import { sitePage } from "@/lib/actions/site";
 
 import { SitePageParams } from "@/types";
 
@@ -10,18 +6,8 @@ const AIChatSettings = async ({
   params: { siteId },
   searchParams,
 }: SitePageParams) => {
-  let site = null;
-  try {
-    site = await sitePage(siteId, true, false);
-  } catch (error) {
-    notFound();
-  }
-  if (!site || !site.siteMap) {
-    notFound();
-  }
-  const siteMap = site?.siteMap;
   const page = parseInt((searchParams?.["kb"] as string) || "0");
-  return <SiteAIPages siteMap={siteMap} siteId={siteId} page={page} />;
+  return <SiteAIPages siteId={siteId} page={page} />;
 };
 
 export default AIChatSettings;
