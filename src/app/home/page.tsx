26396ad8476a9d5a65586c985bb1012b47c2b7b5
Marcus0086 <guptamarcus42@gmail.com>
2024-04-22 00:40:51 +0530

    Update metadata for pages in Dashboard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,33 @@
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
+import { Metadata } from "next";
 
 import { DEMOS } from "@/components/dashboard/constants";
 import LoadingCard from "@/components/dashboard/loadingCard";
 import Hero from "@/components/hero";
-import Pricing from "@/components/pricing";
+import { config } from "@/config";
+
 const HeroParallax = dynamic(() => import("@/components/heroParallax"));
+
+export const metadata: Metadata = {
+  title: config.META_DATA.HOME_PAGE.TITLE,
+  description: config.META_DATA.HOME_PAGE.DESCRIPTION,
+  openGraph: {
+    title: config.META_DATA.HOME_PAGE.TITLE,
+    description: config.META_DATA.HOME_PAGE.DESCRIPTION,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: config.META_DATA.HOME_PAGE.TITLE,
+    description: config.META_DATA.HOME_PAGE.DESCRIPTION,
+  },
+};
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <section className="-mt-[200px]">
+      <section className="">
         <Suspense fallback={<LoadingCard />}>
           <HeroParallax products={DEMOS} />
         </Suspense>
--- src/app/home/pricing/page.tsx
+++ src/app/home/pricing/page.tsx
@@ -1,4 +1,21 @@
+import { Metadata } from "next";
+
 import Pricing from "@/components/pricing";
+import { config } from "@/config";
+
+export const metadata: Metadata = {
+  title: config.META_DATA.PRICING_PAGE.TITLE,
+  description: config.META_DATA.PRICING_PAGE.DESCRIPTION,
+  openGraph: {
+    title: config.META_DATA.PRICING_PAGE.TITLE,
+    description: config.META_DATA.PRICING_PAGE.DESCRIPTION,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: config.META_DATA.PRICING_PAGE.TITLE,
+    description: config.META_DATA.PRICING_PAGE.DESCRIPTION,
+  },
+};
 
 const PricingPage = () => {
   return <Pricing />;
