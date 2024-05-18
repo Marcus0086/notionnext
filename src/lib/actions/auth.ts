cde892fa5148d70fe3371c188630ffdf18526036
Marcus0086 <guptamarcus42@gmail.com>
2024-05-19 02:12:28 +0530

    refactor: Include user creation form data logging in auth.ts
    This commit removes the console.log statement in the createUser function of auth.ts that logged the user creation form data. The console.log statement is no longer needed and has been removed to improve code cleanliness and remove unnecessary logging.

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -7,11 +7,6 @@ const createUser = async (formData: FormData) => {
   const userEmail = formData.get("user_email");
   const userPassword = formData.get("user_password");
   const isKeepLogin = formData.get("user_remember_me");
-  console.log("Create User form data:", {
-    userEmail,
-    userPassword,
-    isKeepLogin,
-  });
 };
 
 const getUserAccount = cache(async () => {
