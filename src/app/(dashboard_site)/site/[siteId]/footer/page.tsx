efe7d29ab0d1b8d2fd383bc066f97052adcb0c43
Marcus0086 <guptamarcus42@gmail.com>
2024-05-19 02:08:40 +0530

    feat: Refactor FooterSettings component to use lazy loading and react-query

--- src/app/(dashboard_site)/site/[siteId]/footer/page.tsx
+++ src/app/(dashboard_site)/site/[siteId]/footer/page.tsx
@@ -1,14 +1,19 @@
 import { Suspense } from "react";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 
 import { FOOTER_SETTINGS } from "@/components/dashboard/constants";
 import LoadingCard from "@/components/dashboard/loadingCard";
 import NameInputCard from "@/components/dashboard/nameInputCard";
+import ToggleInputCard from "@/components/dashboard/toggleInput";
 
 import { getFooterSiteCardById } from "@/lib/actions/site/get/footer";
+import { isCardInput, isToggleInput } from "@/lib/inputs";
+
+import getQueryClient from "@/context/queryClient";
 
 import { SitePageParams } from "@/types/site";
-import { Metadata } from "next";
 
 export async function generateMetadata({
   params: { siteId },
@@ -27,25 +32,46 @@ export async function generateMetadata({
 }
 
 const FooterSettings = async ({ params: { siteId } }: SitePageParams) => {
-  const navSiteCard = await getFooterSiteCardById(siteId);
-  if (!navSiteCard || !navSiteCard.siteConfig) {
+  const footerSiteCard = await getFooterSiteCardById(siteId);
+  if (!footerSiteCard || !footerSiteCard.siteConfig) {
     notFound();
   }
+  const queryClient = getQueryClient();
+  try {
+    await queryClient.prefetchQuery({
+      queryKey: ["footer", siteId],
+      queryFn: () => getFooterSiteCardById(siteId),
+    });
+  } catch (error) {
+    notFound();
+  }
+  const dehydratedState = dehydrate(queryClient);
   return (
-    <>
+    <HydrationBoundary state={dehydratedState}>
       {FOOTER_SETTINGS.map((card, index) => (
         <Suspense key={card.title} fallback={<LoadingCard />}>
-          <NameInputCard
-            key={index}
-            {...card}
-            {...navSiteCard}
-            siteId={siteId}
-            footerStyle={navSiteCard.siteConfig?.footerStyle || "none"}
-            type={card.type}
-          />
+          {isCardInput(card.type) ? (
+            <NameInputCard
+              key={index}
+              {...card}
+              siteId={siteId}
+              footerFootNote={footerSiteCard?.siteConfig?.footer_footnote}
+              footerTitle={footerSiteCard?.siteConfig?.footer_title}
+              footerStyle={footerSiteCard?.siteConfig?.footerStyle}
+              footerIcons={footerSiteCard?.siteConfig?.footerIcons}
+              type={card.type}
+            />
+          ) : isToggleInput(card.type) ? (
+            <ToggleInputCard
+              key={index}
+              {...card}
+              siteId={siteId}
+              type={card.type}
+            />
+          ) : null}
         </Suspense>
       ))}
-    </>
+    </HydrationBoundary>
   );
 };
 
