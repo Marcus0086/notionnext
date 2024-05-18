916c4aaa0a981ccd9327c063ea21801fba5db60d
Marcus0086 <guptamarcus42@gmail.com>
2024-05-19 02:09:43 +0530

    refactor: Update StartFreeDemo component in StartDemoFold to include a highlighted link for starting the demo

--- src/components/startDemoFold.tsx
+++ src/components/startDemoFold.tsx
@@ -9,8 +9,8 @@ import { httpPrefix, domainSuffix } from "@/lib/config";
 
 const StartFreeDemo = () => {
   return (
-    <section className="border-t border-gray-800 rounded-md mt-32">
-      <HeroHighlight className="flex flex-col items-center justify-center w-full gap-y-6">
+    <section className="border-t border-gray-800 rounded-md mt-32 w-full">
+      <HeroHighlight className="flex flex-col items-center justify-center w-full gap-y-2">
         <motion.h2
           initial={{
             opacity: 0,
@@ -24,20 +24,22 @@ const StartFreeDemo = () => {
             duration: 0.5,
             ease: [0.4, 0.0, 0.2, 1],
           }}
-          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-white max-w-5xl leading-relaxed lg:leading-snug text-center mx-auto "
         >
           The new age of websites starts here. <br />
-          <Link
-            href={`${httpPrefix}app.${domainSuffix}`}
-            className="text-2x md:text-4xl lg:text-5xl font-bold text-white hover:underline"
-          >
-            <Highlight className="px-4 py-2">Start Now</Highlight>
-          </Link>
         </motion.h2>
-        <p className="text-lg font-semibold text-center">
+        <p className="max-w-2xl text-sm md:text-base lg:text-lg font-semibold text-center">
           Join community of innovators building hassle-free, scalable websites
           with {config.COMAPNY_NAME}.
         </p>
+        <Highlight className="px-4 py-2 mt-3">
+          <Link
+            href={`${httpPrefix}app.${domainSuffix}`}
+            className="text-2xl font-bold text-white hover:underline"
+          >
+            Start Now
+          </Link>
+        </Highlight>
       </HeroHighlight>
     </section>
   );
