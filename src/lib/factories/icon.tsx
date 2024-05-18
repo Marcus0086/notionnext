3430e002c9eb444ef1e40f7d291bdd55a4857e4c
Marcus0086 <guptamarcus42@gmail.com>
2024-05-19 02:11:16 +0530

    refactor: Update icon factory to include additional social icons
    This commit updates the icon factory in the `icon.tsx` file to include additional social icons such as Facebook, Github, Instagram, and more. This allows for a wider range of social icons to be used throughout the application. The `getSocialIcons` method has been added to retrieve the list of social icons. This change enhances the flexibility and customization options for social media integration.

--- src/lib/factories/icon.tsx
+++ src/lib/factories/icon.tsx
@@ -16,12 +16,29 @@ import { CgFileDocument } from "react-icons/cg";
 import { IoOptionsOutline } from "react-icons/io5";
 import { IconType } from "react-icons";
 import { BsRobot } from "react-icons/bs";
-import { FaLaptopCode } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGooglePay,
+  FaInstagram,
+  FaLaptopCode,
+  FaLinkedinIn,
+  FaNewspaper,
+  FaPinterest,
+  FaReddit,
+  FaSlack,
+  FaSnapchat,
+  FaTelegram,
+  FaTwitch,
+  FaYoutube,
+} from "react-icons/fa6";
 import { MdCreateNewFolder } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import { CiExport } from "react-icons/ci";
 import { MdEditNote } from "react-icons/md";
 import { IoIosGlobe } from "react-icons/io";
+import { FaDiscord, FaTwitter } from "react-icons/fa6";
+import { FaThreads, FaWhatsapp } from "react-icons/fa6";
 
 import { Icons, IconFactory } from "@/types";
 
@@ -48,10 +65,52 @@ class DefaultIconFactory implements IconFactory {
     update: MdEditNote,
     publish: CiExport,
     globe: IoIosGlobe,
+    Discord: FaDiscord,
+    Twitter: FaTwitter,
+    Github: FaGithub,
+    Linkedin: FaLinkedinIn,
+    Newsletter: FaNewspaper,
+    Youtube: FaYoutube,
+    Threads: FaThreads,
+    WhatsApp: FaWhatsapp,
+    Instagram: FaInstagram,
+    Facebook: FaFacebook,
+    Pinterest: FaPinterest,
+    Snapchat: FaSnapchat,
+    Reddit: FaReddit,
+    Slack: FaSlack,
+    Telegram: FaTelegram,
+    Twitch: FaTwitch,
+    GooglePlay: FaGooglePay,
   };
 
   getIcon(icon: Icons): IconType {
     return DefaultIconFactory.iconsFactory[icon];
+  }
+
+  getIconNames(): Icons[] {
+    return Object.keys(DefaultIconFactory.iconsFactory) as Icons[];
+  }
+
+  getSocialIcons(): Icons[] {
+    return [
+      "Facebook",
+      "Github",
+      "GooglePlay",
+      "Instagram",
+      "Linkedin",
+      "Newsletter",
+      "Pinterest",
+      "Reddit",
+      "Slack",
+      "Snapchat",
+      "Telegram",
+      "Threads",
+      "Twitch",
+      "Twitter",
+      "WhatsApp",
+      "Youtube",
+    ];
   }
 }
 
