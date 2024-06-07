abab65ec6e98f8f8ba3bcdca9e9f079f38726892
Marcus0086 <guptamarcus42@gmail.com>
2024-06-08 00:26:32 +0530

    refactor: Add BackgroundBeams component to home layout

--- src/app/(dashboard_app)/dashboard/home/layout.tsx
+++ src/app/(dashboard_app)/dashboard/home/layout.tsx
@@ -4,12 +4,14 @@ import CreateSiteForm from "@/components/dashboard/form";
 import Tabs from "@/components/dashboard/tabs";
 import LoadingCard from "@/components/dashboard/loadingCard";
 import RecentHistory from "@/components/dashboard/recentHistory";
+import { BackgroundBeams } from "@/components/ui/backgroundBeams";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <section className="flex xl:flex-row flex-col items-start justify-between w-full gap-4">
       <section className="pt-5 w-full xl:max-w-[55rem]">
         <div className="rounded-3xl bg-white dark:bg-navy-800 drop-shadow-xl">
+          <BackgroundBeams />
           <CreateSiteForm />
         </div>
         <section className="w-full mt-5">
