83bbc3d8b4be82fd0fc4b0800499ac5be0f9388c
Marcus0086 <guptamarcus42@gmail.com>
2024-08-18 01:36:15 +0530

    refactor: Update getSession and getSessionUser to remove caching

--- src/lib/getSession.ts
+++ src/lib/getSession.ts
@@ -4,20 +4,17 @@ import {
   NextApiRequest,
   NextApiResponse,
 } from "next";
-import { cache } from "react";
 
 import { authOptions } from "@/components/auth/constants";
 
-const getSession = cache(
-  async (
-    ...args:
-      | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
-      | [NextApiRequest, NextApiResponse]
-      | []
-  ) => {
-    const session = await getServerSession(...args, authOptions);
-    return session;
-  },
-);
+const getSession = async (
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) => {
+  const session = await getServerSession(...args, authOptions);
+  return session;
+};
 
 export default getSession;
--- src/lib/getSessionUser.ts
+++ src/lib/getSessionUser.ts
@@ -4,20 +4,17 @@ import {
   NextApiRequest,
   NextApiResponse,
 } from "next";
-import { cache } from "react";
 
 import { authOptions } from "@/components/auth/constants";
 
-const getSessionUser = cache(
-  async (
-    ...args:
-      | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
-      | [NextApiRequest, NextApiResponse]
-      | []
-  ) => {
-    const session = await getServerSession(...args, authOptions);
-    return session?.user;
-  },
-);
+const getSessionUser = async (
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) => {
+  const session = await getServerSession(...args, authOptions);
+  return session?.user;
+};
 
 export default getSessionUser;
