f13b8ea4e58f13960af0a551a76e7143eef33bd2
Marcus0086 <guptamarcus42@gmail.com>
2024-08-18 01:29:37 +0530

    refactor: Update layout.tsx to improve dashboard layout
    This commit updates the layout.tsx file in the dashboard_app directory to improve the dashboard layout. It refactors the code to use a more streamlined and responsive design, including changes to the flexbox structure and border styles. This enhancement enhances the user experience and visual appeal of the dashboard.

--- src/app/(dashboard_app)/layout.tsx
+++ src/app/(dashboard_app)/layout.tsx
@@ -5,20 +5,27 @@ import NavBar from "@/components/dashboard/navBar";
 import AuthContext from "@/components/authContext";
 import { ASIDE_MENU } from "@/components/dashboard/constants";
 
+import { cn } from "@/lib/utils";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NextTopLoader showSpinner={false} />
       <AuthContext>
-        <section className="flex p-6 h-full w-full">
-          <section className="flex rounded-2xl border overflow-hidden drop-shadow-xl">
-            <Aside items={ASIDE_MENU} />
-            <section className="w-full h-full bg-lightPrimary dark:bg-navy-900">
-              <section className="mx-3 flex-none h-full transition-all md:pr-2 md:ml-80">
-                <section className="h-full overflow-auto no-scroll">
-                  <NavBar />
-                  {children}
-                </section>
+        <section className="flex w-full h-full p-2 md:p-6">
+          <Aside items={ASIDE_MENU} />
+          <section
+            className={cn(
+              "w-full h-full",
+              "rounded-2xl border-2 border-lightPrimary dark:border-navy-900",
+              "bg-lightPrimary dark:bg-navy-900",
+              "backdrop-blur-[1px] backdrop-filter dark:backdrop-blur-[1px] dark:backdrop-filter bg-opacity-30 dark:bg-opacity-30"
+            )}
+          >
+            <section className="mx-3 flex-none h-full transition-all md:pr-2 md:ml-72">
+              <section className="h-full overflow-auto no-scroll">
+                <NavBar />
+                {children}
               </section>
             </section>
           </section>
